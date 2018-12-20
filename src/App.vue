d542bb3c51603c0403c353a1e62a5c67d91c41b0
andy.rothwell <rothwell.andy@gmail.com>
2018-12-20 12:57:22 -0500

    comments everyting out to start point

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       <tile-layer v-if="basemap === 'OSM'" :url="'http://{s}.tile.osm.org/{z}/{x}/{y}.png'" />
       <esri-tiled-map-layer v-if="basemap === 'PI'" :url="'//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_2018_3in/MapServer/'" />
       <esri-tiled-map-layer v-if="basemap === 'PI'" :url="'//tiles.arcgis.com/tiles/fLeGjb7u4uXqeF9q/arcgis/rest/services/CityImagery_Labels/MapServer'" />
-      <circle-marker :latlng="[39.952218, -75.163604]"
+      <!-- <circle-marker :latlng="[39.952218, -75.163604]"
                      :radius="8"
                      :color="'black'"
                      :weight="2"
@@ -14,14 +14,14 @@
                      :fillOpacity="1"
                      :opacity="1"
                      v-if="shouldShowCircleMarker"
-      />
+      /> -->
       <!-- <polygon_ :latlngs="[[39.953229, -75.162204],[39.953110, -75.161308],[39.952830, -75.161373],[39.952941, -75.162301],[39.953229, -75.162204]]" /> -->
 
-      <polygon_ :latlngs="this.parcelCoords"
+      <!-- <polygon_ :latlngs="this.parcelCoords"
                 :color="'red'"
                 :weight="2"
                 :fillColor="'blue'"
-      />
+      /> -->
     </map_>
     <!-- <button @click="setShouldShowCircleMarker(true)">Show Circle Marker</button>
     <button @click="setShouldShowCircleMarker(false)">Hide Circle Marker</button>
